78c65c7f6108e0e76656372c661bab84ae7a9a46
Rodion Vagin <letmecut@gmail.com>
2017-06-21 15:52:25 +0300

    Я добавил переменную Б

--- app/src/main/java/com/rodion/myapplication6/MainActivity.java
+++ app/src/main/java/com/rodion/myapplication6/MainActivity.java
@@ -3,6 +3,7 @@ package com.rodion.myapplication6;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 int a;
+int b;
 public class MainActivity extends AppCompatActivity {
 
     @Override
