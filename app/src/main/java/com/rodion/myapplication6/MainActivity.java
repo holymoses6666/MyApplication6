20810d709c29400ee2ebb14c5e7907acc01f9082
Rodion Vagin <letmecut@gmail.com>
2017-06-21 15:56:41 +0300

    Создал класс и перенес переменные

--- app/src/main/java/com/rodion/myapplication6/MainActivity.java
+++ app/src/main/java/com/rodion/myapplication6/MainActivity.java
@@ -2,13 +2,21 @@ package com.rodion.myapplication6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-int a;
-int b;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void TEST ()
+    {
+        int a;
+        int b;
+        a = 1;
+        b = 2;
     }
 }
