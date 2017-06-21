ee28525f79ff51db67083ef0789c2ec4316083d6
Rodion Vagin <letmecut@gmail.com>
2017-06-21 15:48:38 +0300

    Я добавил переменную А

--- app/src/main/java/com/rodion/myapplication6/MainActivity.java
+++ app/src/main/java/com/rodion/myapplication6/MainActivity.java
@@ -2,7 +2,7 @@ package com.rodion.myapplication6;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+int a;
 public class MainActivity extends AppCompatActivity {
 
     @Override
